c9ccf2610021a711bb71c0da0de94b7d6a464e3f
Joseph Nartey <52709529+jnartey2022@users.noreply.github.com>
2020-06-20 15:23:04 -0400

    tried linking new stuff

--- TechrAppAndroidStudio/app/src/main/java/com/example/techrappandroidstudio/MainActivity.java
+++ TechrAppAndroidStudio/app/src/main/java/com/example/techrappandroidstudio/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.techrappandroidstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    Button headsetsbutton;
+    Button micebutton;
+    Button keyboardbutton;
+    Button motherboardbutton;
+    Button cpubutton;
+    Button gpubutton;
+    Button psubutton;
+    Button ssdhddbutton;
+    Button acessoriesbutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        keyboardbutton = findViewById(R.id.keyboardbutton);
+        headsetsbutton = findViewById(R.id.headsetsbutton);
+        micebutton = findViewById(R.id.micebutton);
+        motherboardbutton = findViewById(R.id.motherboardbutton);
+        cpubutton = findViewById(R.id.cpubutton);
+        gpubutton = findViewById(R.id.gpubutton);
+        psubutton = findViewById(R.id.psubutton);
+        ssdhddbutton = findViewById(R.id.ssdhddbutton);
+        acessoriesbutton = findViewById(R.id.acessoriesbutton);
+
+        keyboardbutton.setOnClickListener(goTokeyboardpage());
+        micebutton.setOnClickListener(goTomicepage());
+
+    }
+
+    public void goTokeyboardpage() {
+
+        Intent Tokeyboardpage = new Intent(this, keyboardpage.class);
+        startActivity(Tokeyboardpage);
+    }
+
+    public void goTomicepage() {
+        Intent Tomicepage = new Intent(this, micepage.class);
+        startActivity(Tomicepage);
     }
 }
---
+++ TechrAppAndroidStudio/app/src/main/java/com/example/techrappandroidstudio/micepage.java
@@ -0,0 +1,8 @@
+package com.example.techrappandroidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class micepage extends AppCompatActivity {
+}
